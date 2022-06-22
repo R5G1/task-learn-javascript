fba1699726cdae8aaef8f06fdcbec775b73a45fc
Vladislav <noreply@github.com>
2022-06-22 16:57:13 +0300

    init: start objects task

--- js/сhapter1.js
+++ js/сhapter1.js
@@ -138,4 +138,15 @@ function pow(x,n) {
   return x**n;
 }
 
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+// ask(
+//   "Вы согласны?",
+//   () => console.log("Вы согласились."),
+//   () =>console.log("Вы отменили выполнение.")
+// );
+
 //=========================================================
