80d2e83001b22c2bf4031ba883192be1c3d8fee0
Vladislav <noreply@github.com>
2022-06-27 17:54:55 +0300

    feat: add setTimeout and setInterval task

--- js/сhapter1.js
+++ js/сhapter1.js
@@ -795,4 +795,34 @@ function sum(a) {
 
 // console.log(sum(1)(2));
 
+// setTimeout and setInterval =============================================================================
+
+function printNumbers(from, to) {
+  let current = from;
+
+  let timerId = setInterval(function() {
+    console.log(current);
+    if (current == to) {
+      clearInterval(timerId);
+    }
+    current++;
+  }, 1000);
+}
+
+// printNumbers(5, 10);
+
+function printNumbers(from, to) {
+  let current = from;
+
+  setTimeout(function go() {
+    console.log(current);
+    if (current < to) {
+      setTimeout(go, 1000);
+    }
+    current++;
+  }, 1000);
+}
+
+// printNumbers(5, 10);
+
 // Decorators and redirectors =============================================================================
