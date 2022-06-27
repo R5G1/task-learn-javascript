fc1806b266df91841f2d20a81f4a25e701bf432b
Vladislav <noreply@github.com>
2022-06-27 15:25:07 +0300

    feat: start Object.keys task

--- js/сhapter1.js
+++ js/сhapter1.js
@@ -498,4 +498,25 @@ keys.push('more');
 
 // Object.keys =============================================================================
 
+function sumSalaries(salaries) {
+  let sum = 0;
+  for (let salary of Object.values(salaries)) {
+    sum += salary;
+  }
+
+  return sum; // 650
+}
+
+let salariesA = {
+  John: 100,
+  Pete: 300,
+  Mary: 250,
+};
+
+// console.log(sumSalaries(salariesA));
+
+function count(obj) {
+  return Object.keys(obj).length;
+}
+
 
