0ca8f99f207f1a348f645c580aed5f0a5db44bb4
Vladislav <noreply@github.com>
2022-06-28 16:34:39 +0300

    feat: add document Styles and classes   task

--- js/сhapter2.js
+++ js/сhapter2.js
@@ -140,3 +140,31 @@ function makeSort() {
   tableSort.tBodies[0].append(...sortedRows);
 }
 // makeSort();
+
+// document Styles and classes =============================================================================
+
+function showNotification({ top = 0, right = 0, className, html }) {
+  let notification = document.createElement('div');
+  notification.className = 'notification';
+  if (className) {
+    notification.classList.add(className);
+  }
+  notification.style.position = 'absolute';
+  notification.style.top = top + 'px';
+  notification.style.right = right + 'px';
+
+  notification.innerHTML = html;
+  document.body.append(notification);
+
+  setTimeout(() => notification.remove(), 1500);
+}
+
+// let i = 1;
+// setInterval(() => {
+//   showNotification({
+//     top: 10,
+//     right: 10,
+//     html: 'Hello ' + i++,
+//     className: 'welcome',
+//   });
+// }, 1000);
